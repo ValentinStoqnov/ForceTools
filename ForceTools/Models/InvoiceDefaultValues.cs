97875e1237b84fe26674bf4ae2447a22549788cc
ValentinStoqnov <sqdodo@gmail.com>
2023-08-03 16:49:53 +0300

    Added InvoiceDefaultValues struct.

---
+++ ForceTools/Models/InvoiceDefaultValues.cs
@@ -0,0 +1,11 @@
+﻿namespace ForceTools
+{
+    public struct InvoiceDefaultValues
+    {
+        public int DefaultPurchaseAccount { get; set; }
+        public int DefaultSaleAccount { get; set; }
+        public int DefaultCashRegAccount { get; set; }
+        public string DefaultPurchaseNote { get; set; }
+        public string DefaultSaleNote { get; set; }
+    }
+}
