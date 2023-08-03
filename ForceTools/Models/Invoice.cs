0885d96a6b2590903d4cbc35ea0e934c17e447d6
ValentinStoqnov <sqdodo@gmail.com>
2023-08-03 16:01:13 +0300

    Added Invoice struct.

---
+++ ForceTools/Models/Invoice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForceTools.Models
+{
+    public struct Invoice
+    {
+        public int KontragentId;
+        public DateTime Date;
+        public DateTime AccDate;
+        public long Number;
+        public decimal DO;
+        public decimal DDS;
+        public decimal FullValue;
+        public int AccountingStatusId;
+        public byte[] Image;
+        public int DealKindId;
+        public int DocTypeId;
+        public int InCashAccount;
+        public int Account;
+        public string Note;
+        public string PurchaseOrSale; 
+    }
+}
