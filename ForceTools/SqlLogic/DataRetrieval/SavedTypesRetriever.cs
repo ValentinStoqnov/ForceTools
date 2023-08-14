b169feb142f43fa7532cfd35e894b68e5fc8284d
ValentinStoqnov <sqdodo@gmail.com>
2023-08-14 19:29:54 +0300

    Added dropdown datagrids for DocType and DealType selection.

---
+++ ForceTools/SqlLogic/DataRetrieval/SavedTypesRetriever.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ForceTools
+{
+    public static class SavedTypesRetriever
+    {
+        private static DataTable GetTypesDataTable(string sqlCommandText)
+        {
+            SqlConnection sqlConnection;
+            SqlCommand sqlCommand;
+            SqlDataAdapter sqlDataAdapter;
+            DataTable typesDataTable = new DataTable();
+
+            using (sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlConnectionString"].ConnectionString))
+            {
+                sqlCommand = new SqlCommand(sqlCommandText, sqlConnection);
+                sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                sqlDataAdapter.Fill(typesDataTable);
+            }
+
+            return typesDataTable;
+        }
+        public static DataTable GetDocTypesDataTable()
+        {
+            DataTable docTypesDataTable = GetTypesDataTable("select * from DocumentTypes");
+            return docTypesDataTable;
+        }
+        public static DataTable GetKindOfDealsDataTable()
+        {
+            DataTable kindOfDealsDataTable = GetTypesDataTable("select * from KindOfDeals");
+            return kindOfDealsDataTable;
+        }
+    }
+}
--- ForceTools/WPF Windows/ExcelUploaderWindow.xaml.cs
+++ ForceTools/WPF Windows/ExcelUploaderWindow.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace ForceTools.WPF_Windows
@@ -18,11 +20,19 @@ namespace ForceTools.WPF_Windows
         private DataTable _FinalEditDataTable;
         public DataTable FinalEditDataTable { get { return _FinalEditDataTable; } set { _FinalEditDataTable = value; OnPropertyChanged(); } }
 
+
+        private DataTable _DocTypesDataTable;
+        private DataTable _KindOfDealsDataTable;
+        public DataTable DocTypesDataTable { get {return _DocTypesDataTable; } set { _DocTypesDataTable = value; OnPropertyChanged(); } }
+        public DataTable KindOfDealsDataTable { get { return _KindOfDealsDataTable; } set { _KindOfDealsDataTable = value; OnPropertyChanged(); } }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ExcelUploaderWindow(OperationType operationType)
         {
             _operationType = operationType;
+            DocTypesDataTable = SavedTypesRetriever.GetDocTypesDataTable();
+            KindOfDealsDataTable = SavedTypesRetriever.GetKindOfDealsDataTable();
             InitializeComponent();
         }
         private void CreateComboBoxes()
@@ -163,5 +173,139 @@ namespace ForceTools.WPF_Windows
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        private void DocTypeEditingTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            textBox.Focus();
+            StackPanel stackPanel = textBox.Parent as StackPanel;
+            Popup popup = stackPanel.Children[1] as Popup;
+            DataGrid templateDg = popup.Child as DataGrid;
+            popup.IsOpen = true;
+            SelectRowByIndex(templateDg, 0);
+        }
+        private void FinalEditDataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Up && e.Key != Key.Down && e.Key != Key.Left && e.Key != Key.Right)
+            {
+                if (FinalEditDataGrid.CurrentCell.Column.DisplayIndex == 11) FinalEditDataGrid.BeginEdit();
+                if (FinalEditDataGrid.CurrentCell.Column.DisplayIndex == 12) FinalEditDataGrid.BeginEdit();
+            }
+        }
+        private void DocTypeDataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Up && e.Key != Key.Down && e.Key != Key.Left && e.Key != Key.Right)
+            {
+                DataGrid dg = sender as DataGrid;
+                Popup popup = dg.Parent as Popup;
+                StackPanel stackPanel = popup.Parent as StackPanel;
+                TextBox tb = stackPanel.Children[0] as TextBox;
+                tb.Focus();
+            }
+            if (e.Key == Key.Enter)
+            {
+                DataGrid dg = sender as DataGrid;
+                var selectedItem = DocTypesDataTable.Rows[dg.SelectedIndex][0];
+                FinalEditDataTable.Rows[FinalEditDataGrid.SelectedIndex][11] = selectedItem;
+                Popup popup = dg.Parent as Popup;
+                popup.IsOpen = false;
+                FinalEditDataGrid.CommitEdit();
+            }
+        }
+        private void DealKindEditingTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            textBox.Focus();
+            StackPanel stackPanel = textBox.Parent as StackPanel;
+            Popup popup = stackPanel.Children[1] as Popup;
+            DataGrid templateDg = popup.Child as DataGrid;
+            popup.IsOpen = true;
+            SelectRowByIndex(templateDg, 0);
+        }
+
+        private void DealKindDataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Up && e.Key != Key.Down && e.Key != Key.Left && e.Key != Key.Right)
+            {
+                DataGrid dg = sender as DataGrid;
+                Popup popup = dg.Parent as Popup;
+                StackPanel stackPanel = popup.Parent as StackPanel;
+                TextBox tb = stackPanel.Children[0] as TextBox;
+                tb.Focus();
+            }
+            if (e.Key == Key.Enter)
+            {
+                DataGrid dg = sender as DataGrid;
+                var selectedItem = KindOfDealsDataTable.Rows[dg.SelectedIndex][0];
+                FinalEditDataTable.Rows[FinalEditDataGrid.SelectedIndex][12] = selectedItem;
+                Popup popup = dg.Parent as Popup;
+                popup.IsOpen = false;
+                FinalEditDataGrid.CommitEdit();
+            }
+        }
+        public static void SelectRowByIndex(DataGrid dataGrid, int rowIndex)
+        {
+            try
+            {
+                object item = dataGrid.Items[rowIndex];
+                dataGrid.SelectedItem = item;
+
+                DataGridRow row = dataGrid.ItemContainerGenerator.ContainerFromIndex(rowIndex) as DataGridRow;
+                if (row == null)
+                {
+                    dataGrid.ScrollIntoView(item);
+                    row = dataGrid.ItemContainerGenerator.ContainerFromIndex(rowIndex) as DataGridRow;
+                }
+
+                if (row != null)
+                {
+                    DataGridCell cell = GetCell(dataGrid, row, 0);
+                    if (cell != null)
+                        cell.Focus();
+                }
+            }
+            catch { }
+        }
+        public static DataGridCell GetCell(DataGrid dataGrid, DataGridRow rowContainer, int column)
+        {
+            if (rowContainer != null)
+            {
+                DataGridCellsPresenter presenter = FindVisualChild<DataGridCellsPresenter>(rowContainer);
+                if (presenter == null)
+                {
+                    rowContainer.ApplyTemplate();
+                    presenter = FindVisualChild<DataGridCellsPresenter>(rowContainer);
+                }
+                if (presenter != null)
+                {
+                    DataGridCell cell = presenter.ItemContainerGenerator.ContainerFromIndex(column) as DataGridCell;
+                    if (cell == null)
+                    {
+                        dataGrid.ScrollIntoView(rowContainer, dataGrid.Columns[column]);
+                        cell = presenter.ItemContainerGenerator.ContainerFromIndex(column) as DataGridCell;
+                    }
+                    return cell;
+                }
+            }
+            return null;
+        }
+        public static T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T)
+                    return (T)child;
+                else
+                {
+                    T childOfChild = FindVisualChild<T>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
+        }
+
+        
     }
 }
