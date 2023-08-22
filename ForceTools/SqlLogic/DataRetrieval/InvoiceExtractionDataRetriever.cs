c957920766128dfd2af339b4f758d260b2e283c4
ValentinStoqnov <sqdodo@gmail.com>
2023-08-22 14:25:51 +0300

    Added operation type as an argument to Invoice retriever.

--- ForceTools/SqlLogic/DataRetrieval/InvoiceExtractionDataRetriever.cs
+++ ForceTools/SqlLogic/DataRetrieval/InvoiceExtractionDataRetriever.cs
@@ -11,14 +11,14 @@ namespace ForceTools
         private static SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
         private static DataTable operationsDatatable = new DataTable();
 
-        public static DataTable GetDataTableForExcelFullExport() 
+        public static DataTable GetDataTableForExcelFullExport(OperationType operationType) 
         {
             using (sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlConnectionString"].ConnectionString))
             {
                 operationsDatatable.Clear();
                 sqlConnection.Open();
                 sqlCommand.Connection = sqlConnection;
-                sqlCommand.CommandText = "Select DocumentTypes.TypeName as 'Тип Документ', Fakturi.Number as Номер, Fakturi.Date as Дата, Kontragenti.Name as Контрагент, Kontragenti.EIK as ЕИК, Kontragenti.DDSNumber as  'ДДС Номер' , Fakturi.DO as 'Данъчна основа' , Fakturi.DDS as ДДС , Fakturi.FullValue as Общо ,Fakturi.InCashAccount as 'В брой', Fakturi.Account as Сметка, Fakturi.Note as Бележка, DocumentTypes.Id as 'Код Тип Документ', KindOfDeals.Id as 'Код Тип Сделка', AccountingStatuses.AccountingStatus From Fakturi LEFT JOIN Kontragenti On Fakturi.KontragentiId = Kontragenti.Id LEFT JOIN DocumentTypes on Fakturi.DocTypeId = DocumentTypes.Id LEFT JOIN KindOfDeals on Fakturi.DealKindId = KindOfDeals.Id JOIN AccountingStatuses on Fakturi.AccountingStatusId = AccountingStatuses.Id WHERE AccountingStatusId = 3";
+                sqlCommand.CommandText = $"Select DocumentTypes.TypeName as 'Тип Документ', \r\nFakturi.Number as Номер, Fakturi.Date as Дата, \r\nKontragenti.Name as Контрагент, Kontragenti.EIK as ЕИК, \r\nKontragenti.DDSNumber as  'ДДС Номер' , Fakturi.DO as 'Данъчна основа' , \r\nFakturi.DDS as ДДС , Fakturi.FullValue as Общо ,Fakturi.InCashAccount as 'В брой',\r\nFakturi.Account as Сметка, Fakturi.Note as Бележка, DocumentTypes.Id as 'Код Тип Документ', \r\nKindOfDeals.Id as 'Код Тип Сделка', AccountingStatuses.AccountingStatus From Fakturi LEFT JOIN Kontragenti On \r\nFakturi.KontragentiId = Kontragenti.Id LEFT JOIN DocumentTypes on Fakturi.DocTypeId = DocumentTypes.Id LEFT JOIN KindOfDeals on Fakturi.DealKindId = KindOfDeals.Id \r\nJOIN AccountingStatuses on Fakturi.AccountingStatusId = AccountingStatuses.Id WHERE AccountingStatusId = 3 and Fakturi.PurchaseOrSale = '{operationType.ToString()}'";
                 sqlDataAdapter.Fill(operationsDatatable);
             }
             sqlConnection.Close();
