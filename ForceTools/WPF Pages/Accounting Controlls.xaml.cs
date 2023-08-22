02126a1ef8077a5411729358069b6a73009e88e9
ValentinStoqnov <sqdodo@gmail.com>
2023-08-22 14:26:31 +0300

    Code cleanup and adding operation type arg to a method.

--- ForceTools/WPF Pages/Accounting Controlls.xaml.cs
+++ ForceTools/WPF Pages/Accounting Controlls.xaml.cs
@@ -4,9 +4,6 @@ using System.Windows.Controls;
 
 namespace ForceTools
 {
-    /// <summary>
-    /// Interaction logic for Accounting_Controlls.xaml
-    /// </summary>
     public partial class Accounting_Controlls : Page
     {
         public int NewCount { get; set; }
@@ -15,7 +12,7 @@ namespace ForceTools
         public int ExportedCount { get; set; }
         public int AllCount { get; set; }
 
-        private OperationType OperationType;
+        private OperationType operationType;
 
         public Accounting_Controlls()
         {
@@ -23,14 +20,14 @@ namespace ForceTools
         }
         public Accounting_Controlls(OperationType operationType) : this()
         {
-            OperationType = operationType;
+            this.operationType = operationType;
             GetInvoicesCount();
             SetPurchaseOrSaleLabel();
         }
 
         private void SetPurchaseOrSaleLabel()
         {
-            switch (OperationType)
+            switch (operationType)
             {
                 case OperationType.Purchase:
                     labelPurchasesSales.Content = "Покупки";
@@ -42,36 +39,36 @@ namespace ForceTools
         }
         private void GetInvoicesCount()
         {
-            NewCount = InvoiceCounter.GetNewInvoicesCount(OperationType);
-            UnAcCount = InvoiceCounter.GetUnAccountedInvoicesCount(OperationType);
-            RdyExpCount = InvoiceCounter.GetReadyToBeExportedInvoicesCount(OperationType);
-            ExportedCount = InvoiceCounter.GetExportedInvoicesCount(OperationType);
-            AllCount = InvoiceCounter.GetAllInvoicesCount(OperationType);
+            NewCount = InvoiceCounter.GetNewInvoicesCount(operationType);
+            UnAcCount = InvoiceCounter.GetUnAccountedInvoicesCount(operationType);
+            RdyExpCount = InvoiceCounter.GetReadyToBeExportedInvoicesCount(operationType);
+            ExportedCount = InvoiceCounter.GetExportedInvoicesCount(operationType);
+            AllCount = InvoiceCounter.GetAllInvoicesCount(operationType);
         }
 
         private void AllInvoicesButton_Click(object sender, RoutedEventArgs e)
         {
-            UiNavigationHelper.MainWindow.ContentFrame.Content = new InvoiceGridPage(DocumentStatuses.AccountedDocuments, OperationType);
+            UiNavigationHelper.MainWindow.ContentFrame.Content = new InvoiceGridPage(DocumentStatuses.AccountedDocuments, operationType);
         }
         private void HeldInvoicesButton_Click(object sender, RoutedEventArgs e)
         {
-            UiNavigationHelper.MainWindow.ContentFrame.Content = new InvoiceGridPage(DocumentStatuses.HeldDocuments, OperationType);
+            UiNavigationHelper.MainWindow.ContentFrame.Content = new InvoiceGridPage(DocumentStatuses.HeldDocuments, operationType);
         }
         private void UnAccountedInvoicesBtn_Click(object sender, RoutedEventArgs e)
         {
-            UiNavigationHelper.MainWindow.ContentFrame.Content = new InvoiceGridPage(DocumentStatuses.UnAccountedDocuments, OperationType);
+            UiNavigationHelper.MainWindow.ContentFrame.Content = new InvoiceGridPage(DocumentStatuses.UnAccountedDocuments, operationType);
         }
         private void RdyToBeExportedBtn_Click(object sender, RoutedEventArgs e)
         {
-            UiNavigationHelper.MainWindow.ContentFrame.Content = new InvoiceGridPage(DocumentStatuses.ReadyToBeExportedDocuments, OperationType);
+            UiNavigationHelper.MainWindow.ContentFrame.Content = new InvoiceGridPage(DocumentStatuses.ReadyToBeExportedDocuments, operationType);
         }
         private void ExportedInvoicesBtn_Click(object sender, RoutedEventArgs e)
         {
-            UiNavigationHelper.MainWindow.ContentFrame.Content = new InvoiceGridPage(DocumentStatuses.ExportedDocuments, OperationType);
+            UiNavigationHelper.MainWindow.ContentFrame.Content = new InvoiceGridPage(DocumentStatuses.ExportedDocuments, operationType);
         }
         private void ExportBtn_Click(object sender, RoutedEventArgs e)
         {
-            ExcelExporter.ExtractFromDataTable(InvoiceExtractionDataRetriever.GetDataTableForExcelFullExport());
+            ExcelExporter.ExtractFromDataTable(InvoiceExtractionDataRetriever.GetDataTableForExcelFullExport(operationType));
         }
     }
 }
